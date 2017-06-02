a5afb42cda76cc812731b601cb47792564f0eee1
Taras Ignashchenko <tignashchenko@gmail.com>
2017-06-02 12:12:06 -0700

    Refactor GroceryListItem to a class component

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-var GroceryListItem = (props) => (
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <li>{this.props.todos}</li>
+    )
+  }
+}
+
+/*var GroceryListItem = (props) => (
   <ul>
     <li>{props.todos}</li>
   </ul>
-);
+);*/
 
 var GroceryList = (props) => (
   <div>
