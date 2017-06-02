ee8c4858d8267b6c43bf52ae4475a031f82403b3
Taras Ignashchenko <tignashchenko@gmail.com>
2017-06-02 12:06:05 -0700

    Refactor GroceryList to use GroceryListItem component

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-var GroceryList = () => (
+var GroceryListItem = (props) => (
   <ul>
-    <Meat />
-  <Vegetables />
+    <li>{props.todos}</li>
   </ul>
 );
 
-var Meat = () => (
-  <li>Meat</li>
-);
-
-var Vegetables = () => (
-  <li>Vegetables</li>
+var GroceryList = (props) => (
+  <div>
+    <h2>Grocery List</h2>
+  <GroceryListItem todos={['Meat']}/>
+<GroceryListItem todos={['Vegetables']}/>
+  </div>
 );
 
 ReactDOM.render(<GroceryList />, document.getElementById('app'));
